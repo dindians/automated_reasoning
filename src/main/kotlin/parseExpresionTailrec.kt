e2e9376e9c8bda3f3666a7c0f728bbb15725dcab
Anton de Craen <anton.decraen@unifiedpost.com>
2020-12-30 09:34:54 +0100

    rename function parameter parseExpresionTailrec.kt

--- src/main/kotlin/parseExpresionTailrec.kt
+++ src/main/kotlin/parseExpresionTailrec.kt
@@ -1,16 +1,16 @@
-internal tailrec fun parseExpressionTailrec(tokens: List<String>, makeExpression: (Expression) -> Expression = { it }): Pair<Expression, List<String>> {
-    val (expression, unparsedTokens) = parseProduct(tokens, makeExpression)
+internal tailrec fun parseExpressionTailrec(tokens: List<String>, accumulateExpression: (Expression) -> Expression = { it }): Pair<Expression, List<String>> {
+    val (expression, unparsedTokens) = parseProduct(tokens, accumulateExpression)
     return when {
         unparsedTokens.isEmpty() || unparsedTokens.first() != "+" -> Pair(expression, unparsedTokens)
         else -> parseExpressionTailrec(unparsedTokens.drop(1)) { (Expression.Add(expression, it)) }
     }
 }
 
-private tailrec fun parseProduct(tokens: List<String>, makeExpression: (Expression) -> Expression): Pair<Expression, List<String>> {
+private tailrec fun parseProduct(tokens: List<String>, accumulateExpression: (Expression) -> Expression): Pair<Expression, List<String>> {
     val (expression, unparsedTokens) = parseAtom(tokens)
     return when {
-        unparsedTokens.isEmpty() || unparsedTokens.first() != "*" -> Pair(makeExpression(expression), unparsedTokens)
-        else -> parseProduct(unparsedTokens.drop(1)) { makeExpression(Expression.Mul(expression, it)) }
+        unparsedTokens.isEmpty() || unparsedTokens.first() != "*" -> Pair(accumulateExpression(expression), unparsedTokens)
+        else -> parseProduct(unparsedTokens.drop(1)) { accumulateExpression(Expression.Mul(expression, it)) }
     }
 }
 
@@ -20,7 +20,7 @@ private fun parseAtom(tokens: List<String>): Pair<Expression, List<String>> {
     val remainingTokens = tokens.drop(1)
     return when (firstToken) {
         "(" -> {
-            val (expression, unparsedTokens) = parseExpressionTailrec(remainingTokens) { it }
+            val (expression, unparsedTokens) = parseExpressionTailrec(remainingTokens)
             if(unparsedTokens.firstOrNull() != ")") throw MissingClosingBracketException("Expected closing bracket (actual: ${unparsedTokens.firstOrNull()})")
             Pair(expression, unparsedTokens.drop(1).toMutableList())
         }
