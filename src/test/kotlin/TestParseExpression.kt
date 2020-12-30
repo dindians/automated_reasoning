3885b465df507418897d19e23f4ca2da37736d28
Anton de Craen <anton.decraen@unifiedpost.com>
2020-12-30 09:21:23 +0100

    more parser tests

--- src/test/kotlin/TestParseExpression.kt
+++ src/test/kotlin/TestParseExpression.kt
@@ -11,6 +11,7 @@ internal class TestParseExpression {
     fun `parse 0`() {
         with(parseExpression(listOf("0"))) {
             println(this)
+            assertTrue("remaining tokens is empty") { second.isEmpty() }
             assertEquals(constant(0), this.first)
         }
     }
@@ -19,6 +20,7 @@ internal class TestParseExpression {
     fun `parse 00`() {
         with(parseExpression(listOf("00"))) {
             println(this)
+            assertTrue("remaining tokens is empty") { second.isEmpty() }
             assertEquals(constant(0), this.first)
         }
     }
@@ -27,6 +29,7 @@ internal class TestParseExpression {
     fun `parse 01`() {
         with(parseExpression(listOf("01"))) {
             println(this)
+            assertTrue("remaining tokens is empty") { second.isEmpty() }
             assertEquals(constant(1), this.first)
         }
     }
@@ -35,6 +38,7 @@ internal class TestParseExpression {
     fun `parse 234`() {
         with(parseExpression(listOf("234"))) {
             println(this)
+            assertTrue("remaining tokens is empty") { second.isEmpty() }
             assertEquals(constant(234), this.first)
         }
     }
@@ -43,6 +47,7 @@ internal class TestParseExpression {
     fun `parse x`() {
         with(parseExpression(listOf("x"))) {
             println(this)
+            assertTrue("remaining tokens is empty") { second.isEmpty() }
             assertEquals(variable("x"), this.first)
         }
     }
@@ -51,14 +56,16 @@ internal class TestParseExpression {
     fun `parse xyz`() {
         with(parseExpression(listOf("xyz"))) {
             println(this)
+            assertTrue("remaining tokens is empty") { second.isEmpty() }
             assertEquals(variable("xyz"), this.first)
         }
     }
 
     @Test
-    fun `parse (0)`() {
+    fun `parse _0)`() {
         with(parseExpression(listOf("(", "0", ")"))) {
             println(this)
+            assertTrue("remaining tokens is empty") { second.isEmpty() }
             assertEquals(constant(0), this.first)
         }
     }
@@ -79,9 +86,10 @@ internal class TestParseExpression {
     }
 
     @Test
-    fun `parse ((0))`() {
+    fun `parse __0))`() {
         with(parseExpression(listOf("(", "(", "0", ")", ")"))) {
             println(this)
+            assertTrue("remaining tokens is empty") { second.isEmpty() }
             assertEquals(constant(0), this.first)
         }
     }
@@ -90,6 +98,7 @@ internal class TestParseExpression {
     fun `parse 0x_a'2e`() {
         with(parseExpression(listOf("0x_a'2e"))) {
             println(this)
+            assertTrue("remaining tokens is empty") { second.isEmpty() }
             assertEquals(variable("0x_a'2e"), this.first)
         }
     }
@@ -98,6 +107,7 @@ internal class TestParseExpression {
     fun `parse 0 + 0`() {
         with(parseExpression(listOf("0", "+", "0"))) {
             println(this)
+            assertTrue("remaining tokens is empty") { second.isEmpty() }
             assertTrue { first is Expression.Add }
             with(first as Expression.Add) {
                 assertEquals(constant(0), expr1)
@@ -110,6 +120,7 @@ internal class TestParseExpression {
     fun `parse 13 + 27`() {
         with(parseExpression(listOf("13", "+", "27"))) {
             println(this)
+            assertTrue("remaining tokens is empty") { second.isEmpty() }
             assertTrue { first is Expression.Add }
             with(first as Expression.Add) {
                 assertEquals(constant(13), expr1)
@@ -119,9 +130,10 @@ internal class TestParseExpression {
     }
 
     @Test
-    fun `parse (13 + 27)`() {
+    fun `parse _13 + 27)`() {
         with(parseExpression(listOf("(", "13", "+", "27", ")"))) {
             println(this)
+            assertTrue("remaining tokens is empty") { second.isEmpty() }
             assertTrue { first is Expression.Add }
             with(first as Expression.Add) {
                 assertEquals(constant(13), expr1)
@@ -131,9 +143,10 @@ internal class TestParseExpression {
     }
 
     @Test
-    fun `parse (13) + (27)`() {
+    fun `parse _13) + _27)`() {
         with(parseExpression(listOf("(", "13", ")", "+", "(", "27", ")"))) {
             println(this)
+            assertTrue("remaining tokens is empty") { second.isEmpty() }
             assertTrue { first is Expression.Add }
             with(first as Expression.Add) {
                 assertEquals(constant(13), expr1)
@@ -143,9 +156,10 @@ internal class TestParseExpression {
     }
 
     @Test
-    fun `parse ((13) + (27)(`() {
+    fun `parse __13) + _27))`() {
         with(parseExpression(listOf("(", "(", "13", ")", "+", "(", "27", ")", ")"))) {
             println(this)
+            assertTrue("remaining tokens is empty") { second.isEmpty() }
             assertTrue { first is Expression.Add }
             with(first as Expression.Add) {
                 assertEquals(constant(13), expr1)
@@ -158,6 +172,7 @@ internal class TestParseExpression {
     fun `parse x + 5`() {
         with(parseExpression(listOf("x", "+", "5"))) {
             println(this)
+            assertTrue("remaining tokens is empty") { second.isEmpty() }
             assertTrue { first is Expression.Add }
             with(first as Expression.Add) {
                 assertEquals(variable("x"), expr1)
@@ -167,9 +182,10 @@ internal class TestParseExpression {
     }
 
     @Test
-    fun `parse (x + 5)`() {
+    fun `parse _x + 5)`() {
         with(parseExpression(listOf("(", "x", "+", "5", ")"))) {
             println(this)
+            assertTrue("remaining tokens is empty") { second.isEmpty() }
             assertTrue("first expression is add") { first is Expression.Add }
             with(first as Expression.Add) {
                 assertEquals(variable("x"), expr1)
@@ -179,13 +195,65 @@ internal class TestParseExpression {
     }
 
     @Test
-    fun `parse 2 x (x + 5)`() {
+    fun `parse x + y + z`() {
+        with(parseExpression(listOf("x", "+", "y", "+", "z"))) {
+            println(this)
+            assertTrue("remaining tokens is empty") { second.isEmpty() }
+            assertTrue("first expression is add") { first is Expression.Add }
+            with(first as Expression.Add) {
+                assertTrue("second expression is add") { expr1 is Expression.Add }
+                with(expr1 as Expression.Add) {
+                    assertEquals(variable("x"), expr1)
+                    assertEquals(variable("y"), expr2)
+                }
+                assertEquals(variable("z"), expr2)
+            }
+        }
+    }
+
+    @Test
+    fun `parse (x + y) + z`() {
+        with(parseExpression(listOf("(", "x", "+", "y", ")", "+", "z"))) {
+            println(this)
+            assertTrue("remaining tokens is empty") { second.isEmpty() }
+            assertTrue("first expression is add") { first is Expression.Add }
+            with(first as Expression.Add) {
+                assertTrue("second expression is add") { expr1 is Expression.Add }
+                with(expr1 as Expression.Add) {
+                    assertEquals(variable("x"), expr1)
+                    assertEquals(variable("y"), expr2)
+                }
+                assertEquals(variable("z"), expr2)
+            }
+        }
+    }
+
+    @Test
+    fun `parse x + _y + z)`() {
+        with(parseExpression(listOf("x", "+", "(", "y", "+", "z", ")"))) {
+            println(this)
+            assertTrue("remaining tokens is empty") { second.isEmpty() }
+            assertTrue("first expression is add") { first is Expression.Add }
+            with(first as Expression.Add) {
+                assertEquals(variable("x"), expr1)
+                assertTrue("second expression is add") { expr2 is Expression.Add }
+                with(expr2 as Expression.Add) {
+                    assertEquals(variable("y"), expr1)
+                    assertEquals(variable("z"), expr2)
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `parse 2 times _x + 5)`() {
         with(parseExpression(listOf("2", "*", "(", "x", "+", "5", ")"))) {
             println(this)
+            assertTrue("remaining tokens is empty") { second.isEmpty() }
             assertTrue("first expression is multiply") { first is Expression.Mul }
             with(first as Expression.Mul) {
                 assertEquals(constant(2), expr1)
-                assertTrue("second expression is multiply") { expr2 is Expression.Add }
+                assertTrue("second expression is add") { expr2 is Expression.Add }
                 with(expr2 as Expression.Add) {
                     assertEquals(variable("x"), expr1)
                     assertEquals(constant(5), expr2)
@@ -200,6 +268,7 @@ internal class TestParseExpression {
         val randomInt = Random.nextInt(0, 4)
         with(parseExpression(listOf(randomVariable, "+", randomInt.toString()))) {
             println(this)
+            assertTrue("remaining tokens is empty") { second.isEmpty() }
             assertTrue { first is Expression.Add }
             with(first as Expression.Add) {
                 assertEquals(variable(randomVariable), expr1)
@@ -214,6 +283,7 @@ internal class TestParseExpression {
         val randomInt = Random.nextInt(0, 4)
         with(parseExpression(listOf(randomVariable, "*", randomInt.toString()))) {
             println(this)
+            assertTrue("remaining tokens is empty") { second.isEmpty() }
             assertTrue { first is Expression.Mul }
             with(first as Expression.Mul) {
                 assertEquals(variable(randomVariable), expr1)
@@ -228,9 +298,27 @@ internal class TestParseExpression {
             println(this)
             assertTrue { first is Expression.Add }
             with(first as Expression.Add) {
+                assertTrue("remaining tokens is empty") { second.isEmpty() }
+                assertTrue { expr1 is Expression.Add }
+                with(expr1 as Expression.Add) {
+                    assertEquals(constant(36), expr1)
+                    assertEquals(constant(4), expr2)
+                }
+                assertEquals(constant(73), expr2)
+            }
+        }
+    }
+
+    @Test
+    fun `parse 36 times 4 times 73`() {
+        with(parseExpression(listOf("36", "*", "4", "*", "73"))) {
+            println(this)
+            assertTrue("remaining tokens is empty") { second.isEmpty() }
+            assertTrue { first is Expression.Mul }
+            with(first as Expression.Mul) {
                 assertEquals(constant(36), expr1)
-                assertTrue { expr2 is Expression.Add }
-                with(expr2 as Expression.Add) {
+                assertTrue { expr2 is Expression.Mul }
+                with(expr2 as Expression.Mul) {
                     assertEquals(constant(4), expr1)
                     assertEquals(constant(73), expr2)
                 }
