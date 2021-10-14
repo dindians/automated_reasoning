c7d8d040419fe5c283ec17fe0faffbd767b279ff
Anton de Craen <anton.decraen@unifiedpost.com>
2021-10-14 07:56:04 +0200

    structure unit tests

--- src/main/kotlin/automated_reasoning/lex.kt
+++ src/main/kotlin/automated_reasoning/lex.kt
@@ -1,4 +1,5 @@
-package automated_reasoning/*
+package automated_reasoning
+/*
  * lexical analysis of a string
  * parses a tokenized string into a list of tokens
  * recursively extracts a token from the beginning of the string and add the extracted token to the beginning of the token list
@@ -16,10 +17,10 @@ package automated_reasoning/*
  * a parser turns a concrete syntax into abstract syntax (e.g. AST, abstract syntax tree)
  * a pretty-printer does the reverse
  */
-tailrec fun lex(inp: String, tokens: MutableList<String> = mutableListOf()): MutableList<String> {
+tailrec fun lex(inp: String, tokens: MutableList<String> = mutableListOf()): List<String> {
     val inpMinusSpaces = lexWhile(inp = inp, prop = space).second
     return when {
-        inpMinusSpaces.isEmpty() -> tokens
+        inpMinusSpaces.isEmpty() -> tokens.toList()
         else -> {
             val (token, inpMinusToken) = inpMinusSpaces[0].let { char -> lexWhile(char.toString(), inpMinusSpaces.drop(1), match(char)) }
             lex(inpMinusToken, tokens.apply { add(token) })
@@ -32,17 +33,26 @@ private tailrec fun lexWhile(token: String = "", inp: String, prop: (Char) -> Bo
     else -> lexWhile(token + inp[0], inp.drop(1), prop)
 }
 
+/**
+ * Here's a possible alternative to the lexWhile() fun
+ * see also public inline fun CharSequence.dropWhile(predicate: (Char) -> Boolean): CharSequence
+ */
+private inline fun String.myDropWhile(predicate: (Char) -> Boolean): Pair<String, String> {
+    for (index in indices) if(!predicate(this[index])) return Pair(substring(0 until index), substring(index))
+    return Pair(this, "")
+}
+
 private fun match(value: Char) = when {
     alphanumeric(value) -> alphanumeric
     symbolic(value) -> symbolic
-//                    automated_reasoning.space(it) -> { _ -> false }
-//                    automated_reasoning.punctuation(it) -> { _ -> false }
+//                    space(it) -> { _ -> false }
+//                    punctuation(it) -> { _ -> false }
 //                    else -> throw Exception("lexical analysis encountered invalid character '$it' after parsing '${tokens.joinToString(",")}'.")
-    else -> { _ -> false }
+    else -> { _ -> false }  // no match
 }
 
 private val space: (Char) -> Boolean = { " \t\n\r".matches(it) }
-private val punctuation: (Char) -> Boolean = { "()[]{}".matches(it) }
+//private val punctuation: (Char) -> Boolean = { "()[]{}".matches(it) }
 private val symbolic: (Char) -> Boolean = { "~`!@#$%^&*-+=|\\:;<>.?/".matches(it) }
 internal val numeric: (Char) -> Boolean = { "0123456789".matches(it) }
 private val alphanumeric: (Char) -> Boolean = { "abcdefghijklmnopqrstuvwxyz_'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".matches(it) }
